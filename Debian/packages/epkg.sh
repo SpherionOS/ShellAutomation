995e837d10dee09d2a7d616e86ab0d47f2ec67a6
SpherionOS <122868668+SpherionOS@users.noreply.github.com>
2023-03-06 22:02:31 +1000

    Update epkg.sh

--- Debian/packages/epkg.sh
+++ Debian/packages/epkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y openssh-client vim git curl wget docker.io
+sudo apt install -y openssh-client vim git curl wget
 
 echo "Essential pkgs installed and updated"
