8822e6e9ed1f7e4ba7221230f540c33763112737
SpherionOS <122868668+SpherionOS@users.noreply.github.com>
2023-05-14 00:50:26 +1000

    Create recon1.ps1

---
+++ Windows/powershell/recon/recon1.ps1
@@ -0,0 +1,17 @@
+$systype = systeminfo | findstr "System"
+$dns = ipconfig /all | findstr "DNS"
+$mem = systeminfo | findstr "Memory"
+
+$content = $systype + $mem + $dns + $ip
+$content | out-file -filepath ~/Downloads/ImportantInfo.txt
+
+clear
+
+ if($content = $true)
+{
+read-host -prompt "File successfully exported to ~/Downloads/ImportantInfo.txt given that it existed at the time"
+}
+else
+{
+read-host -prompt "Task failed quite unsuccessfully"
+}
